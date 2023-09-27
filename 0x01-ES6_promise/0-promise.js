ca6f6c0a42b4257b374fc00ee2130b3f22836b59
Frank <wanyoike39@gmail.com>
2023-09-27 21:12:54 +0300

    other changes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
+/* module defining a promise function */
 export default function getResponseFromAPI() {
-    const login = true;
-    return new Promise((resolve, reject) => {
-        if (login) {
-          resolve()
-        } else {
-          reject();
-        }
-    });
+    return Promise.resolve();
 }
