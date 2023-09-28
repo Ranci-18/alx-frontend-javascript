41f7333a8cb3f9391497c5879f8bf0c7a177271a
Frank <wanyoike39@gmail.com>
2023-09-28 07:34:14 +0300

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((settledPromises) => {
+      const array = [];
+      for (const eachSettledPromise of settledPromises) {
+        array.push({status: eachSettledPromise.status, value: eachSettledPromise.value || eachSettledPromise.reason})
+      }
+      return array;
+    });
+}
