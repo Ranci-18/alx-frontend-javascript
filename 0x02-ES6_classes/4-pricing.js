7cb8b68e23f9e8e2e49706c01dc17d44335ca71b
Frank <wanyoike39@gmail.com>
2023-09-28 13:21:36 +0300

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw TypeError('Currency must be an instance of class Currency');
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) throw TypeError('Currency must be an instance of class Currency');
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    if (typeof conversionRate !== 'number') throw TypeError('CoversionRate must be a number');
+    return amount * conversionRate;
+  }
+}
