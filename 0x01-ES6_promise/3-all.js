8f091af973eb1ae31ca069576730ee993549021a
Frank <wanyoike39@gmail.com>
2023-09-28 07:02:42 +0300

    task 4 and 5

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-      .then((property) => {
-        console.log(`${property[0].body} ${property[1].firstName} ${property[1].lastName}`)
-      })
-      .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((property) => {
+      console.log(`${property[0].body} ${property[1].firstName} ${property[1].lastName}`)
+    })
+    .catch(() => console.log('Signup system offline'));
 }
