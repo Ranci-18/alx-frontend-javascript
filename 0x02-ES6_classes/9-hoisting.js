facde21b03d99dde534d4c9946bb9139cc9b44af
Frank <wanyoike39@gmail.com>
2023-09-28 21:52:16 +0300

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -18,7 +18,7 @@ const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
