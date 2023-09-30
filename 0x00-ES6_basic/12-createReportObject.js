ba16092b511b5ce339668b3cd6062ddf3651c2e3
Frank <wanyoike39@gmail.com>
2023-09-30 15:02:39 +0300

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,8 @@
-import createEmployeesObject from './11-createEmployeesObject';
-
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeesList,
     },
-    getNumberOfDepartments: (employeesList) => {
-      Object.keys(employeesList).length;
-    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
   };
 }
