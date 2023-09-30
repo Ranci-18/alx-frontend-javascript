c14decd2734f7c50f8c4387f502a1f0e65dd0600
Frank <wanyoike39@gmail.com>
2023-09-30 17:28:27 +0300

    eslint fixes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,10 +2,10 @@
 export default function getResponseFromAPI() {
   const login = true;
   return new Promise((resolve, reject) => {
-      if (login) {
-        resolve();
-      } else {
-        reject();
-      }
+    if (login) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((property) => {
-      console.log(`${property[0].body} ${property[1].firstName} ${property[1].lastName}`)
+      console.log(`${property[0].body} ${property[1].firstName} ${property[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
