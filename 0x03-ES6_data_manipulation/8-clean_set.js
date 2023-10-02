03909caabd42f4017ceb112cbb3390cc3dd1671c
Frank <wanyoike39@gmail.com>
2023-10-02 18:33:20 +0300

    task 8 fix 3

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
   const finalList = [];
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (value && value.startsWith(startString)) {
       finalList.push(value.slice(startString.length));
     }
   }
