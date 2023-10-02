ba198cf76e922e8cbb3ccb29b6927aa9b5779871
Frank <wanyoike39@gmail.com>
2023-10-02 18:26:35 +0300

    task 8 fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
   const setArray = [...set];
   const filteredSet = setArray.filter((element) => element.startsWith(startString));
   const cleanedSet = filteredSet.map((element) => element.slice(startString.length));
